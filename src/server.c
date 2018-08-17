720e6b9ff9078717a6cc9bfa10f145c828d25343
jingliu9 <jingliu.cs@gmail.com>
2018-08-17 14:11:39 -0700

    comment the rewrite file`

--- src/server.c
+++ src/server.c
@@ -1128,10 +1128,12 @@ int serverCron(struct aeEventLoop *eventLoop, long long id, void *clientData) {
             long long base = server.aof_rewrite_base_size ?
                             server.aof_rewrite_base_size : 1;
             long long growth = (server.aof_current_size*100/base) - 100;
+            // LIBOSSPDK: we turn of rewrite here
+            /**
             if (growth >= server.aof_rewrite_perc) {
                 serverLog(LL_NOTICE,"Starting automatic rewriting of AOF on %lld%% growth",growth);
                 rewriteAppendOnlyFileBackground();
-            }
+            }**/
          }
     }
 
